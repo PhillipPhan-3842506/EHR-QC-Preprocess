147b8dbe2b004fcfef7ba741be22f1439cb5f8db
ryashpal <ryashpal89@gmail.com>
2023-11-02 03:29:40 +0000

    isolation forst outlier utility

--- ehrqc/plot/OutlierIsolationForest.py
+++ ehrqc/plot/OutlierIsolationForest.py
@@ -1,10 +1,7 @@
-import os, tempfile, subprocess
 import time
 import base64
 
 import pandas as pd
-import numpy as np
-from scipy import stats
 
 from pathlib import Path
 
@@ -15,6 +12,52 @@ from yattag import Doc
 doc, tag, text = Doc().tagtext()
 
 from ehrqc import Settings
+
+
+def getOutlierScores(df):
+    from sklearn.ensemble import IsolationForest
+
+    iforest = IsolationForest(n_estimators=100)
+    return iforest.fit_predict(df)
+
+def __drawOutliers(df, columns, outputFile):
+
+    from matplotlib import pyplot as plt
+    import seaborn as sns
+
+    dataDf = df[columns]
+
+    outlier_scores = getOutlierScores(dataDf)
+
+    if len(columns) < 2:
+        log.info('Can not generate outlier plot due to insufficient number of attributes (<2)')
+        return
+    elif len(columns) == 2:
+        plotdf = dataDf
+    else:
+        plotdf = pd.DataFrame(PCA(2).fit(dataDf).transform(dataDf), columns=['component_1', 'component_2'])
+    
+    plotdf['outlier_scores'] = outlier_scores
+
+    if plotdf is not None and len(plotdf) > 1:
+        fig, ax = plt.subplots()
+        x = plotdf.columns[0]
+        y = plotdf.columns[1]
+        sns.scatterplot(data=plotdf, x=x, y=y, hue='outlier_scores')
+
+        ax.set_title('Scatter plot')
+        ax.set_xlabel(x)
+        ax.set_ylabel(y)
+
+        encoded = None
+        outPath = Path(Path(outputFile).parent, 'outlier_plot_' + x + '_' + y + '.png')
+        fig.savefig(outPath, format='png', bbox_inches='tight')
+        with open(outPath, "rb") as outFile:
+            encoded = base64.b64encode(outFile.read()).decode('utf-8')
+    else:
+        encoded = None
+
+    return encoded
 
 
 def clean(source_file, save_file, columns):
@@ -35,22 +78,20 @@ def clean(source_file, save_file, columns):
     log.info('Validating complete!!')
 
     log.info('Removing outliers')
-    dataDf = dataDf[columns]
-    if dataDf is not None:
-        if not dataDf.empty:
-            outliersDf = irt_ensemble(dataDf)
-            outliersDf = outliersDf[(np.abs(stats.zscore(outliersDf.ensemble_scores)) < Settings.ensemble_score_threshold)]
-            outliersDf.drop('y_knn_agg', axis=1, inplace=True)
-            outliersDf.drop('y_lof', axis=1, inplace=True)
-            outliersDf.drop('y_inflo', axis=1, inplace=True)
-            outliersDf.drop('y_kdeos', axis=1, inplace=True)
-            outliersDf.drop('y_ldf', axis=1, inplace=True)
-            outliersDf.drop('ensemble_scores', axis=1, inplace=True)
+    df = dataDf[columns]
+    if df is not None:
+        if not df.empty:
+            outlier_scores = getOutlierScores(df)
+            dataDf['outlier_scores'] = outlier_scores
+            correctedDf = dataDf[dataDf.outlier_scores == 1]
+            correctedDf.drop(columns=['outlier_scores'], inplace=True)
 
-    outliersDf.to_csv(outputFile, index=False)
+    log.info('Saving the corrected file')
+    if (correctedDf is not None) and (not correctedDf.empty):
+        correctedDf.to_csv(outputFile, index=False)
 
 
-def visualise(source_file, save_file, combinations, columns):
+def visualise(source_file, save_file, columns):
 
     outputFile = Path(save_file)
     dataDf = pd.read_csv(source_file)
@@ -58,9 +99,6 @@ def visualise(source_file, save_file, combinations, columns):
 
     if(columns and (dataDf[columns].shape[1] > Settings.col_limit_for_outlier_plot)):
         log.info('Too many variables to plot!! Please sleect the vaiables to plot using combinations argument.')
-        return
-    elif(combinations and (len(combinations) > Settings.combinations_limit_for_outlier_plot)):
-        log.info('Too many variables to plot!! The number of combination should not be more than 6.')
         return
     elif (dataDf.shape[0] * dataDf.shape[1]) > int(Settings.cell_limit):
         log.info('This file has ' + str(dataDf.shape[0] * dataDf.shape[1]) + ' cells.')
@@ -77,21 +115,7 @@ def visualise(source_file, save_file, combinations, columns):
     with tag('html'):
         doc.asis('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">')
         with tag('body'):
-            if combinations:
-                for (x, y) in combinations:
-                    if x in dataDf.columns and y in dataDf.columns:
-                        with tag('div'):
-                            with tag('h1'):
-                                doc.asis('<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>')
-                                with tag('span', klass='fs-4', style="margin: 10px;"):
-                                    text('Outliers plot - ' + x + ' - ' + y)
-                                    log.info('Generating outlier plot for columns: ' + x + ' - ' + y)
-                            fig = __drawOutliers(dataDf, [x, y], outputFile)
-                            if fig:
-                                with tag('div', style="float: left;"):
-                                    doc.asis('<img src=\'data:image/png;base64,{}\'>'.format(fig))
-                    doc.asis('<div style="clear:both;"></div>')
-            elif columns:
+            if columns:
                 with tag('div'):
                     with tag('h1'):
                         doc.asis('<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>')
@@ -117,72 +141,13 @@ def visualise(source_file, save_file, combinations, columns):
     log.info('Completed writing output file!!')
 
 
-def __drawOutliers(df, columns, outputFile):
-
-    from matplotlib import pyplot as plt
-    import seaborn as sns
-
-    if len(columns) < 2:
-        log.info('Can not generate outlier plot due to insufficient number of attributes (<2)')
-    elif len(columns) == 2:
-        plotdf = df[columns]
-    else:
-        plotdf = pd.DataFrame(PCA(2).fit(df).transform(df), columns=['component_1', 'component_2'])
-    outliersDf = irt_ensemble(plotdf)
-
-    if outliersDf is not None and len(outliersDf) > 1:
-        fig, ax = plt.subplots()
-        x_o = outliersDf.columns[0].replace(' ', '.')
-        y_o = outliersDf.columns[1].replace(' ', '.')
-        sns.scatterplot(data=outliersDf, x=x_o, y=y_o, hue='ensemble_scores')
-
-        ax.set_title('Scatter plot')
-        ax.set_xlabel(outliersDf.columns[0])
-        ax.set_ylabel(outliersDf.columns[1])
-
-        encoded = None
-        outPath = Path(Path(outputFile).parent, 'outlier_plot_' + outliersDf.columns[0] + '_' + outliersDf.columns[1] + '.png')
-        fig.savefig(outPath, format='png', bbox_inches='tight')
-        with open(outPath, "rb") as outFile:
-            encoded = base64.b64encode(outFile.read()).decode('utf-8')
-    else:
-        encoded = None
-
-    return encoded
-
-
-def irt_ensemble(data):
-
-    inFile = tempfile.NamedTemporaryFile(delete=False)
-    outFile = tempfile.NamedTemporaryFile(delete=False)
-    try:
-        data.to_csv(inFile, index=False)
-        subprocess.call (["/usr/bin/Rscript", "--vanilla", "ehrqc/utils/script.r", inFile.name, outFile.name])
-
-        if outFile:
-            try:
-                out = pd.read_csv(outFile)
-            except:
-                out = None
-        else:
-            out = None
-
-    finally:
-        inFile.close()
-        outFile.close()
-        os.unlink(inFile.name)
-        os.unlink(outFile.name)
-
-    return out
-
-
 if __name__ == '__main__':
 
     import logging
     import sys
     import argparse
 
-    log = logging.getLogger("EHRQC")
+    log = logging.getLogger("EHR-QC")
     log.setLevel(logging.INFO)
     format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
@@ -192,7 +157,7 @@ if __name__ == '__main__':
 
     log.info("Parsing command line arguments")
 
-    parser = argparse.ArgumentParser(description='EHRQC')
+    parser = argparse.ArgumentParser(description='Outlier graphs using IRT ensemble technique')
 
     parser.add_argument('source_file', nargs=1, default='./data.csv',
                         help='Source data file path')
@@ -203,9 +168,6 @@ if __name__ == '__main__':
     parser.add_argument('action', nargs=1, default='visualise',
                         help='Action to perform [visualise, clean]')
 
-    parser.add_argument('-com', '--combinations', nargs='*', action='append',
-                        help='Column combinations to plot - can have multiple column pairs (required only for action=visualise).')
-
     parser.add_argument('-col', '--columns', nargs='*',
                         help='Column names to be used for outlier detection - must have two or more column names (required for both actions i.e. visualise and clean).')
 
@@ -214,12 +176,11 @@ if __name__ == '__main__':
     log.info('args.source_file: ' + str(args.source_file[0]))
     log.info('args.save_file: ' + str(args.save_file[0]))
     log.info('args.action: ' + str(args.action[0]))
-    log.info('args.combinations: ' + str(args.combinations))
     log.info('args.columns: ' + str(args.columns))
 
     if args.action[0] == 'clean':
         clean(source_file=args.source_file[0], save_file=args.save_file[0], columns=args.columns)
     else:
-        visualise(source_file=args.source_file[0], save_file=args.save_file[0], combinations=args.combinations, columns=args.columns)
+        visualise(source_file=args.source_file[0], save_file=args.save_file[0], columns=args.columns)
 
     log.info('Done!!')
