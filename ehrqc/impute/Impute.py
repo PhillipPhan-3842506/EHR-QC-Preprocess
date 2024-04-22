9405a1c3007350ab1236242c83faad1d79f5e8e8
Phillip Phan <s3842506@student.rmit.edu.au>
2024-04-22 22:10:13 +1000

    fix miss_forest max_columns

--- ehrqc/impute/Impute.py
+++ ehrqc/impute/Impute.py
@@ -8,7 +8,8 @@ import re
 
 import numpy as np
 import pandas as pd
-
+import os
+from pathlib import Path
 from sklearn.metrics import r2_score
 
 import impyute as impy
@@ -19,6 +20,7 @@ from sklearn.impute import SimpleImputer
 from ehrqc.utils.MissForest import MissForest
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 
@@ -26,90 +28,110 @@ def compare(fullDf, p=None):
 
     if not p:
         if fullDf.isna().sum().mean() > 0:
-            p = fullDf.isna().sum().mean()/fullDf.shape[0]
+            p = fullDf.isna().sum().mean() / fullDf.shape[0]
     else:
         p = float(p[0])
 
-    log.info('Creating missingness with proportion p = (' + str(p) + ')')
+    log.info("Creating missingness with proportion p = (" + str(p) + ")")
 
-    log.info('Data size before dropping nulls: ' + str(fullDf.shape))
+    log.info("Data size before dropping nulls: " + str(fullDf.shape))
     fullDf.dropna(inplace=True)
-    log.info('Data size after dropping nulls: ' + str(fullDf.shape))
+    log.info("Data size after dropping nulls: " + str(fullDf.shape))
 
-    mask = np.random.choice(a=[True, False], size=fullDf.shape, p=[p, 1-p])
+    mask = np.random.choice(a=[True, False], size=fullDf.shape, p=[p, 1 - p])
     missingDf = fullDf.mask(mask)
-    missingDf = missingDf.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+    missingDf = missingDf.rename(columns=lambda x: re.sub("[^A-Za-z0-9_]+", "", x))
 
     try:
-        log.info('Running mean imputation')
-        meanImputer = SimpleImputer(strategy='mean')
+        log.info("Running mean imputation")
+        meanImputer = SimpleImputer(strategy="mean")
         meanImputedData = meanImputer.fit_transform(missingDf)
-        meanImputedDf = pd.DataFrame(meanImputedData, columns=[(col + '_mean') for col in missingDf.columns], index=missingDf.index)
+        meanImputedDf = pd.DataFrame(
+            meanImputedData,
+            columns=[(col + "_mean") for col in missingDf.columns],
+            index=missingDf.index,
+        )
         meanR2 = r2_score(fullDf, meanImputedDf)
-        log.info('Mean imputation R2 score: ' + str(meanR2))
+        log.info("Mean imputation R2 score: " + str(meanR2))
     except:
-        log.exception('Mean imputation failed!!')
+        log.exception("Mean imputation failed!!")
         meanR2 = 0
 
-
     try:
-        log.info('Running median imputation')
-        medianImputer = SimpleImputer(strategy='median')
+        log.info("Running median imputation")
+        medianImputer = SimpleImputer(strategy="median")
         medianImputedData = medianImputer.fit_transform(missingDf)
-        medianImputedDf = pd.DataFrame(medianImputedData, columns=[(col + '_median') for col in missingDf.columns], index=missingDf.index)
+        medianImputedDf = pd.DataFrame(
+            medianImputedData,
+            columns=[(col + "_median") for col in missingDf.columns],
+            index=missingDf.index,
+        )
         medianR2 = r2_score(fullDf, medianImputedDf)
-        log.info('Median imputation R2 score: ' + str(medianR2))
+        log.info("Median imputation R2 score: " + str(medianR2))
     except:
-        log.exception('Median imputation failed!!')
+        log.exception("Median imputation failed!!")
         medianR2 = 0
 
     try:
-        log.info('Running KNN imputation')
+        log.info("Running KNN imputation")
         knnImputer = KNNImputer()
         knnImputedData = knnImputer.fit_transform(missingDf)
-        knnImputedDf = pd.DataFrame(knnImputedData, columns=[(col + '_knn') for col in missingDf.columns], index=missingDf.index)
+        knnImputedDf = pd.DataFrame(
+            knnImputedData,
+            columns=[(col + "_knn") for col in missingDf.columns],
+            index=missingDf.index,
+        )
         knnR2 = r2_score(fullDf, knnImputedDf)
-        log.info('KNN imputation R2 score: ' + str(knnR2))
+        log.info("KNN imputation R2 score: " + str(knnR2))
     except:
-        log.exception('KNN imputation failed!!')
+        log.exception("KNN imputation failed!!")
         knnR2 = 0
 
     try:
-        log.info('Running MissForest imputation')
+        log.info("Running MissForest imputation")
         mfImputer = MissForest()
         mfImputedData = mfImputer.fit(missingDf).transform(missingDf)
-        mfImputedDf = pd.DataFrame(mfImputedData, columns=[(col + '_mf') for col in missingDf.columns], index=missingDf.index)
+        mfImputedDf = pd.DataFrame(
+            mfImputedData,
+            columns=[(col + "_mf") for col in missingDf.columns],
+            index=missingDf.index,
+        )
         mfR2 = r2_score(fullDf, mfImputedDf)
-        log.info('MissForest imputation R2 score: ' + str(mfR2))
+        log.info("MissForest imputation R2 score: " + str(mfR2))
     except:
-        log.exception('MissForest imputation failed!!')
+        log.exception("MissForest imputation failed!!")
         mfR2 = 0
 
     try:
-        log.info('Running EM imputation')
+        log.info("Running EM imputation")
         emImputedData = impy.em(missingDf.to_numpy())
-        emImputedDataDf = pd.DataFrame(emImputedData, columns=[(col + '_em') for col in missingDf.columns], index=missingDf.index)
+        emImputedDataDf = pd.DataFrame(
+            emImputedData,
+            columns=[(col + "_em") for col in missingDf.columns],
+            index=missingDf.index,
+        )
         emR2 = r2_score(fullDf, emImputedDataDf)
-        log.info('EM imputation R2 score: ' + str(emR2))
+        log.info("EM imputation R2 score: " + str(emR2))
     except:
-        log.exception('EM imputation failed!!')
+        log.exception("EM imputation failed!!")
         emR2 = 0
 
     try:
-        log.info('Running MI imputation')
+        log.info("Running MI imputation")
         miKernel = mf.ImputationKernel(
-        missingDf,
-        datasets=4,
-        save_all_iterations=True,
-        random_state=1
+            missingDf, datasets=4, save_all_iterations=True, random_state=1
         )
         miKernel.mice(2)
         miImputedDataDf = miKernel.complete_data(dataset=0, inplace=False)
-        miImputedDf = pd.DataFrame(miImputedDataDf.to_numpy(), columns=[(col + '_mi') for col in missingDf.columns], index=missingDf.index)
+        miImputedDf = pd.DataFrame(
+            miImputedDataDf.to_numpy(),
+            columns=[(col + "_mi") for col in missingDf.columns],
+            index=missingDf.index,
+        )
         miR2 = r2_score(fullDf, miImputedDf)
-        log.info('MI imputation R2 score: ' + str(miR2))
+        log.info("MI imputation R2 score: " + str(miR2))
     except:
-        log.exception('MI imputation failed!!')
+        log.exception("MI imputation failed!!")
         miR2 = 0
 
     return meanR2, medianR2, knnR2, mfR2, emR2, miR2
@@ -118,54 +140,92 @@ def compare(fullDf, p=None):
 def impute(dataDf, algorithm):
 
     imputedDf = None
-    if algorithm == 'mean':
-        meanImputer = SimpleImputer(strategy='mean')
+    if algorithm == "mean":
+        meanImputer = SimpleImputer(strategy="mean")
         meanImputedData = meanImputer.fit_transform(dataDf)
-        imputedDf = pd.DataFrame(meanImputedData, columns=dataDf.columns, index=dataDf.index)
-    if algorithm == 'median':
-        medianImputer = SimpleImputer(strategy='median')
+        imputedDf = pd.DataFrame(
+            meanImputedData, columns=dataDf.columns, index=dataDf.index
+        )
+    if algorithm == "median":
+        medianImputer = SimpleImputer(strategy="median")
         medianImputedData = medianImputer.fit_transform(dataDf)
-        imputedDf = pd.DataFrame(medianImputedData, columns=dataDf.columns, index=dataDf.index)
-    if algorithm == 'knn':
+        imputedDf = pd.DataFrame(
+            medianImputedData, columns=dataDf.columns, index=dataDf.index
+        )
+    if algorithm == "knn":
         knnImputer = KNNImputer()
         knnImputedData = knnImputer.fit_transform(dataDf)
-        imputedDf = pd.DataFrame(knnImputedData, columns=dataDf.columns, index=dataDf.index)
-    if algorithm == 'miss_forest':
+        imputedDf = pd.DataFrame(
+            knnImputedData, columns=dataDf.columns, index=dataDf.index
+        )
+    if algorithm == "miss_forest":
         mfImputer = MissForest()
         mfImputedData = mfImputer.fit(dataDf).transform(dataDf)
-        imputedDf = pd.DataFrame(mfImputedData, columns=dataDf.columns, index=dataDf.index)
-    if algorithm == 'expectation_maximization':
+        imputedDf = pd.DataFrame(
+            mfImputedData, columns=dataDf.columns, index=dataDf.index
+        )
+    if algorithm == "expectation_maximization":
         emImputedData = impy.em(dataDf.to_numpy())
-        imputedDf = pd.DataFrame(emImputedData, columns=dataDf.columns, index=dataDf.index)
-    if algorithm == 'multiple_imputation':
+        imputedDf = pd.DataFrame(
+            emImputedData, columns=dataDf.columns, index=dataDf.index
+        )
+    if algorithm == "multiple_imputation":
         miKernel = mf.ImputationKernel(
-        dataDf,
-        datasets=4,
-        save_all_iterations=True,
-        random_state=1
+            dataDf, datasets=4, save_all_iterations=True, random_state=1
         )
         miKernel.mice(2)
         miImputedDataDf = miKernel.complete_data(dataset=0, inplace=False)
-        imputedDf = pd.DataFrame(miImputedDataDf.to_numpy(), columns=dataDf.columns, index=dataDf.index)
+        imputedDf = pd.DataFrame(
+            miImputedDataDf.to_numpy(), columns=dataDf.columns, index=dataDf.index
+        )
 
     return imputedDf
 
 
-def run(action='compare', source_path = 'data.csv', p=None, save_path = 'imputed.csv', algorithm = 'mean', columns=[]):
+def run(
+    action="compare",
+    source_path="data.csv",
+    p=None,
+    save_path="imputed.csv",
+    algorithm="mean",
+    columns=[],
+):
     dataDf = pd.read_csv(source_path)
+    print(dataDf.columns)
 
     if columns and len(columns) > 0:
-        if(action=='compare'):
-            meanR2, medianR2, knnR2, mfR2, emR2, miR2 = compare(fullDf=dataDf[columns], p=p)
-            log.info('mean R2: ' + str(meanR2) + ', median R2: ' + str(medianR2) + ', knn R2: ' + str(knnR2) + ', mf R2: ' + str(mfR2) + ', em R2: ' + str(emR2) + ', mi R2: ' + str(miR2))
-        elif(action=='impute'):
+        if action == "compare":
+            meanR2, medianR2, knnR2, mfR2, emR2, miR2 = compare(
+                fullDf=dataDf[columns], p=p
+            )
+            log.info(
+                "mean R2: "
+                + str(meanR2)
+                + ", median R2: "
+                + str(medianR2)
+                + ", knn R2: "
+                + str(knnR2)
+                + ", mf R2: "
+                + str(mfR2)
+                + ", em R2: "
+                + str(emR2)
+                + ", mi R2: "
+                + str(miR2)
+            )
+        elif action == "impute":
             imputedDf = impute(dataDf=dataDf[columns], algorithm=algorithm)
             otherDf = dataDf[dataDf.columns[~dataDf.columns.isin(columns)]]
             finalDf = pd.concat([otherDf, imputedDf], axis=1)
-            finalDf.to_csv(save_path, index=None)
+
+            dirPath = Path(save_path).parent
+            if not os.path.exists(dirPath):
+                os.makedirs(dirPath)
+
+            finalDf.to_csv(Path(save_path), index=None)
+            # finalDf.to_csv(save_path, index=None)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     import logging
     import sys
@@ -181,42 +241,61 @@ if __name__ == '__main__':
 
     log.info("Parsing command line arguments")
 
-    parser = argparse.ArgumentParser(description='EHR-QC')
+    parser = argparse.ArgumentParser(description="EHR-QC")
 
-    parser.add_argument('source_path', nargs=1, default='data.csv',
-                        help='Source data path')
+    parser.add_argument(
+        "source_path", nargs=1, default="data.csv", help="Source data path"
+    )
 
-    parser.add_argument('-ac', '--action', nargs=1, default='compare',
-                        help='Action to perform [compare, impute]')
+    parser.add_argument(
+        "-ac",
+        "--action",
+        nargs=1,
+        default="compare",
+        help="Action to perform [compare, impute]",
+    )
 
-    parser.add_argument('-p', '--percentage', nargs=1,
-                        help='Missing value proportion for comparison (required only for action=compare)')
+    parser.add_argument(
+        "-p",
+        "--percentage",
+        nargs=1,
+        help="Missing value proportion for comparison (required only for action=compare)",
+    )
 
-    parser.add_argument('-sp', '--save_path', nargs=1, default='imputed.csv',
-                        help='Path of the file to store the outputs (required only for action=impute)')
+    parser.add_argument(
+        "-sp",
+        "--save_path",
+        nargs=1,
+        default="imputed.csv",
+        help="Path of the file to store the outputs (required only for action=impute)",
+    )
 
-    parser.add_argument('-al', '--algorithm', nargs=1, default='mean',
-                        help='Missing data imputation algorithm [mean, median, knn, miss_forest, expectation_maximization, multiple_imputation]')
+    parser.add_argument(
+        "-al",
+        "--algorithm",
+        nargs=1,
+        default="mean",
+        help="Missing data imputation algorithm [mean, median, knn, miss_forest, expectation_maximization, multiple_imputation]",
+    )
 
-    parser.add_argument('-c', '--columns', nargs='*',
-                        help='Columns to impute')
+    parser.add_argument("-c", "--columns", nargs="*", help="Columns to impute")
 
     args = parser.parse_args()
 
-    log.info('args.action: ' + str(args.action[0]))
-    log.info('args.source_path: ' + str(args.source_path[0]))
-    log.info('args.percentage: ' + str(args.percentage))
-    log.info('args.save_path: ' + str(args.save_path[0]))
-    log.info('args.algorithm: ' + str(args.algorithm[0]))
-    log.info('args.columns: ' + str(args.columns))
+    log.info("args.action: " + str(args.action[0]))
+    log.info("args.source_path: " + str(args.source_path[0]))
+    log.info("args.percentage: " + str(args.percentage))
+    log.info("args.save_path: " + str(args.save_path[0]))
+    log.info("args.algorithm: " + str(args.algorithm[0]))
+    log.info("args.columns: " + str(args.columns))
 
     run(
         action=args.action[0],
-        source_path = args.source_path[0],
-        p = args.percentage,
-        save_path = args.save_path[0],
-        algorithm = args.algorithm[0],
-        columns=args.columns
-        )
+        source_path=args.source_path[0],
+        p=args.percentage,
+        save_path=args.save_path[0],
+        algorithm=args.algorithm[0],
+        columns=args.columns,
+    )
 
-    log.info('Done!!')
+    log.info("Done!!")
--- ehrqc/utils/MissForest.py
+++ ehrqc/utils/MissForest.py
@@ -10,6 +10,7 @@ import numpy as np
 # Author: Ashim Bhattarai
 # License: GNU General Public License v3 (GPLv3)
 
+
 # from .pairwise_external import _get_mask
 def _get_mask(X, value_to_mask):
     """Compute the boolean mask X == missing_values."""
@@ -20,7 +21,7 @@ def _get_mask(X, value_to_mask):
 
 
 __all__ = [
-    'MissForest',
+    "MissForest",
 ]
 
 
@@ -211,13 +212,29 @@ class MissForest(BaseEstimator, TransformerMixin):
            [8.  , 8. , 7. ]])
     """
 
-    def __init__(self, max_iter=10, decreasing=False, missing_values=np.nan,
-                 copy=True, n_estimators=100, criterion=('squared_error', 'gini'),
-                 max_depth=None, min_samples_split=2, min_samples_leaf=1,
-                 min_weight_fraction_leaf=0.0, max_features='auto',
-                 max_leaf_nodes=None, min_impurity_decrease=0.0,
-                 bootstrap=True, oob_score=False, n_jobs=-1, random_state=None,
-                 verbose=0, warm_start=False, class_weight=None):
+    def __init__(
+        self,
+        max_iter=10,
+        decreasing=False,
+        missing_values=np.nan,
+        copy=True,
+        n_estimators=100,
+        criterion=("squared_error", "gini"),
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_features=1.0,
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        bootstrap=True,
+        oob_score=False,
+        n_jobs=-1,
+        random_state=None,
+        verbose=0,
+        warm_start=False,
+        class_weight=None,
+    ):
 
         self.max_iter = max_iter
         self.decreasing = decreasing
@@ -257,13 +274,15 @@ class MissForest(BaseEstimator, TransformerMixin):
 
             # Make initial guess for missing values
             col_means = np.full(Ximp.shape[1], fill_value=np.nan)
-            col_means[self.num_vars_] = self.statistics_.get('col_means')
+            col_means[self.num_vars_] = self.statistics_.get("col_means")
             Ximp[missing_num_rows, missing_num_cols] = np.take(
-                col_means, missing_num_cols)
+                col_means, missing_num_cols
+            )
 
             # Reg criterion
-            reg_criterion = self.criterion if type(self.criterion) == str \
-                else self.criterion[0]
+            reg_criterion = (
+                self.criterion if type(self.criterion) == str else self.criterion[0]
+            )
 
             # Instantiate regression model
             rf_regressor = RandomForestRegressor(
@@ -281,7 +300,8 @@ class MissForest(BaseEstimator, TransformerMixin):
                 n_jobs=self.n_jobs,
                 random_state=self.random_state,
                 verbose=self.verbose,
-                warm_start=self.warm_start)
+                warm_start=self.warm_start,
+            )
 
         # If needed, repeat for categorical variables
         if self.cat_vars_ is not None:
@@ -295,12 +315,15 @@ class MissForest(BaseEstimator, TransformerMixin):
 
             # Make initial guess for missing values
             col_modes = np.full(Ximp.shape[1], fill_value=np.nan)
-            col_modes[self.cat_vars_] = self.statistics_.get('col_modes')
-            Ximp[missing_cat_rows, missing_cat_cols] = np.take(col_modes, missing_cat_cols)
+            col_modes[self.cat_vars_] = self.statistics_.get("col_modes")
+            Ximp[missing_cat_rows, missing_cat_cols] = np.take(
+                col_modes, missing_cat_cols
+            )
 
             # Classfication criterion
-            clf_criterion = self.criterion if type(self.criterion) == str \
-                else self.criterion[1]
+            clf_criterion = (
+                self.criterion if type(self.criterion) == str else self.criterion[1]
+            )
 
             # Instantiate classification model
             rf_classifier = RandomForestClassifier(
@@ -319,7 +342,8 @@ class MissForest(BaseEstimator, TransformerMixin):
                 random_state=self.random_state,
                 verbose=self.verbose,
                 warm_start=self.warm_start,
-                class_weight=self.class_weight)
+                class_weight=self.class_weight,
+            )
 
         # 2. misscount_idx: sorted indices of cols in X based on missing count
         misscount_idx = np.argsort(col_missing_count)
@@ -336,8 +360,8 @@ class MissForest(BaseEstimator, TransformerMixin):
         col_index = np.arange(Ximp.shape[1])
 
         while (
-                gamma_new < gamma_old or gamma_newcat < gamma_oldcat) and \
-                self.iter_count_ < self.max_iter:
+            gamma_new < gamma_old or gamma_newcat < gamma_oldcat
+        ) and self.iter_count_ < self.max_iter:
 
             # 4. store previously imputed matrix
             Ximp_old = np.copy(Ximp)
@@ -380,10 +404,14 @@ class MissForest(BaseEstimator, TransformerMixin):
 
             # 9. Update gamma (stopping criterion)
             if self.cat_vars_ is not None:
-                gamma_newcat = np.sum(
-                    (Ximp[:, self.cat_vars_] != Ximp_old[:, self.cat_vars_])) / n_catmissing
+                gamma_newcat = (
+                    np.sum((Ximp[:, self.cat_vars_] != Ximp_old[:, self.cat_vars_]))
+                    / n_catmissing
+                )
             if self.num_vars_ is not None:
-                gamma_new = np.sum((Ximp[:, self.num_vars_] - Ximp_old[:, self.num_vars_]) ** 2) / np.sum((Ximp[:, self.num_vars_]) ** 2)
+                gamma_new = np.sum(
+                    (Ximp[:, self.num_vars_] - Ximp_old[:, self.num_vars_]) ** 2
+                ) / np.sum((Ximp[:, self.num_vars_]) ** 2)
 
             print("Iteration:", self.iter_count_)
             self.iter_count_ += 1
@@ -408,11 +436,15 @@ class MissForest(BaseEstimator, TransformerMixin):
         """
 
         # Check data integrity and calling arguments
-        force_all_finite = False if self.missing_values in ["NaN",
-                                                            np.nan] else True
+        force_all_finite = False if self.missing_values in ["NaN", np.nan] else True
 
-        X = check_array(X, accept_sparse=False, dtype=np.float64,
-                        force_all_finite=force_all_finite, copy=self.copy)
+        X = check_array(
+            X,
+            accept_sparse=False,
+            dtype=np.float64,
+            force_all_finite=force_all_finite,
+            copy=self.copy,
+        )
 
         # Check for +/- inf
         if np.any(np.isinf(X)):
@@ -430,25 +462,28 @@ class MissForest(BaseEstimator, TransformerMixin):
             elif type(cat_vars) == list or type(cat_vars) == np.ndarray:
                 if np.array(cat_vars).dtype != int:
                     raise ValueError(
-                        "cat_vars needs to be either an int or an array "
-                        "of ints.")
+                        "cat_vars needs to be either an int or an array " "of ints."
+                    )
             else:
-                raise ValueError("cat_vars needs to be either an int or an array "
-                                 "of ints.")
+                raise ValueError(
+                    "cat_vars needs to be either an int or an array " "of ints."
+                )
 
         # Identify numerical variables
         num_vars = np.setdiff1d(np.arange(X.shape[1]), cat_vars)
         num_vars = num_vars if len(num_vars) > 0 else None
 
         # First replace missing values with NaN if it is something else
-        if self.missing_values not in ['NaN', np.nan]:
+        if self.missing_values not in ["NaN", np.nan]:
             X[np.where(X == self.missing_values)] = np.nan
 
         # Now, make initial guess for missing values
         col_means = np.nanmean(X[:, num_vars], axis=0) if num_vars is not None else None
-        col_modes = mode(
-            X[:, cat_vars], axis=0, nan_policy='omit')[0] if cat_vars is not \
-                                                           None else None
+        col_modes = (
+            mode(X[:, cat_vars], axis=0, nan_policy="omit")[0]
+            if cat_vars is not None
+            else None
+        )
 
         self.cat_vars_ = cat_vars
         self.num_vars_ = num_vars
@@ -471,10 +506,14 @@ class MissForest(BaseEstimator, TransformerMixin):
         check_is_fitted(self, ["cat_vars_", "num_vars_", "statistics_"])
 
         # Check data integrity
-        force_all_finite = False if self.missing_values in ["NaN",
-                                                            np.nan] else True
-        X = check_array(X, accept_sparse=False, dtype=np.float64,
-                        force_all_finite=force_all_finite, copy=self.copy)
+        force_all_finite = False if self.missing_values in ["NaN", np.nan] else True
+        X = check_array(
+            X,
+            accept_sparse=False,
+            dtype=np.float64,
+            force_all_finite=force_all_finite,
+            copy=self.copy,
+        )
 
         # Check for +/- inf
         if np.any(np.isinf(X)):
@@ -486,19 +525,21 @@ class MissForest(BaseEstimator, TransformerMixin):
             raise ValueError("One or more columns have all rows missing.")
 
         # Get fitted X col count and ensure correct dimension
-        n_cols_fit_X = (0 if self.num_vars_ is None else len(self.num_vars_)) \
-            + (0 if self.cat_vars_ is None else len(self.cat_vars_))
+        n_cols_fit_X = (0 if self.num_vars_ is None else len(self.num_vars_)) + (
+            0 if self.cat_vars_ is None else len(self.cat_vars_)
+        )
         _, n_cols_X = X.shape
 
         if n_cols_X != n_cols_fit_X:
-            raise ValueError("Incompatible dimension between the fitted "
-                             "dataset and the one to be transformed.")
+            raise ValueError(
+                "Incompatible dimension between the fitted "
+                "dataset and the one to be transformed."
+            )
 
-        # Check if anything is actually missing and if not return original X                             
+        # Check if anything is actually missing and if not return original X
         mask = _get_mask(X, self.missing_values)
         if not mask.sum() > 0:
-            warnings.warn("No missing value located; returning original "
-                          "dataset.")
+            warnings.warn("No missing value located; returning original " "dataset.")
             return X
 
         # row_total_missing = mask.sum(axis=1)
