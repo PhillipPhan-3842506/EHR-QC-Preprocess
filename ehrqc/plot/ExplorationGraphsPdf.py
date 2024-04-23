d09f4dc5af8573b22eabaed78d30a76e66b9ec9d
Phillip Phan <s3842506@student.rmit.edu.au>
2024-04-23 22:38:04 +1000

    changes for ehrqc-web

--- ehrqc/plot/ExplorationGraphsPdf.py
+++ ehrqc/plot/ExplorationGraphsPdf.py
@@ -12,14 +12,21 @@ import seaborn as sns
 from PIL import Image
 
 from fpdf import FPDF
+import json
+import logging
+import sys
+import argparse
+
+log = logging.getLogger("EHR-QC")
+log.setLevel(logging.INFO)
+format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setFormatter(format)
+log.addHandler(ch)
 
 
-def plot(
-    sourceFiles,
-    colNamesDict,
-    outputFile,
-    labels
-    ):
+def plot(sourceFiles, colNamesDict, outputFile, labels):
 
     log.info("Reading the source files")
     sourceDfs = []
@@ -30,7 +37,6 @@ def plot(
             sourceDfs.append(df)
 
     pdf = FPDF()
-
     xSize = len(sourceDfs)
     for key in colNamesDict.keys():
         log.info("Plotting: " + str(key))
@@ -38,27 +44,22 @@ def plot(
         fig, ax = plt.subplots()
         for i in range(xSize):
             y = sourceDfs[i][colNamesDict[key][i]]
-            df = pd.DataFrame({'x': labels[i], 'y': y})
+            df = pd.DataFrame({"x": labels[i], "y": y})
             plotDfs.append(df)
         plotDf = pd.concat(plotDfs, ignore_index=True)
-        sns.violinplot(
-            data=plotDf
-            , x='x'
-            , y='y'
-            , ax=ax
-        )
+        sns.violinplot(data=plotDf, x="x", y="y", ax=ax)
 
-        ax.set_title('Distribution Plot')
+        ax.set_title("Distribution Plot")
         ax.set_xlabel(str(key))
-        ax.set_ylabel('Value')
+        ax.set_ylabel("Value")
 
         # Converting Figure to an image:
         canvas = FigureCanvas(fig)
         canvas.draw()
         img = Image.fromarray(np.asarray(canvas.buffer_rgba()))
 
-        pdf.add_page(orientation='landscape')
-        pdf.image(img, w=pdf.epw * 0.8)  # Make the image full width
+        pdf.add_page(orientation="landscape")
+        pdf.image(img, w=pdf.w * 0.8)  # Make the image full width
 
     log.info("Saving the pdf")
     dirPath = Path(outputFile).parent
@@ -67,41 +68,37 @@ def plot(
     pdf.output(outputFile)
 
 
-if __name__ == '__main__':
-
-    import json
-    import logging
-    import sys
-    import argparse
-
-    log = logging.getLogger("EHR-QC")
-    log.setLevel(logging.INFO)
-    format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setFormatter(format)
-    log.addHandler(ch)
+if __name__ == "__main__":
 
     log.info("Parsing command line arguments")
 
-    parser = argparse.ArgumentParser(description='Draw exploration graphs as pdf files')
+    parser = argparse.ArgumentParser(description="Draw exploration graphs as pdf files")
 
-    parser.add_argument('save_path', nargs=1, default='plot.pdf',
-                        help='Path of the file to store the output')
+    parser.add_argument(
+        "save_path",
+        nargs=1,
+        default="plot.pdf",
+        help="Path of the file to store the output",
+    )
 
-    parser.add_argument('-c', '--columns', type=json.loads)
+    parser.add_argument("-c", "--columns", type=json.loads)
 
-    parser.add_argument('-sf', '--source_file_list', nargs='*')
+    parser.add_argument("-sf", "--source_file_list", nargs="*")
 
-    parser.add_argument('-l', '--labels', nargs='*')
+    parser.add_argument("-l", "--labels", nargs="*")
 
     args = parser.parse_args()
 
-    log.info('args.save_path: ' + str(args.save_path[0]))
-    log.info('args.columns: ' + str(args.columns))
-    log.info('args.source_file_list: ' + str(args.source_file_list))
-    log.info('args.labels: ' + str(args.labels))
+    log.info("args.save_path: " + str(args.save_path[0]))
+    log.info("args.columns: " + str(args.columns))
+    log.info("args.source_file_list: " + str(args.source_file_list))
+    log.info("args.labels: " + str(args.labels))
 
-    plot(sourceFiles=args.source_file_list, colNamesDict=args.columns, outputFile=args.save_path[0], labels=args.labels)
+    plot(
+        sourceFiles=args.source_file_list,
+        colNamesDict=args.columns,
+        outputFile=args.save_path[0],
+        labels=args.labels,
+    )
 
-    log.info('Done!!')
+    log.info("Done!!")
--- ehrqc/plot/OutlierIsolationForest.py
+++ ehrqc/plot/OutlierIsolationForest.py
@@ -14,12 +14,29 @@ doc, tag, text = Doc().tagtext()
 
 from ehrqc import Settings
 
+import logging
+import sys
+import argparse
+
+log = logging.getLogger("EHR-QC")
+log.setLevel(logging.INFO)
+format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setFormatter(format)
+log.addHandler(ch)
+
+log.info("Parsing command line arguments")
+
 
 def getOutlierScores(df):
     from sklearn.ensemble import IsolationForest
 
-    iforest = IsolationForest(n_estimators=100, contamination=Settings.outlier_contamination)
+    iforest = IsolationForest(
+        n_estimators=100, contamination=Settings.outlier_contamination
+    )
     return iforest.fit_predict(df)
+
 
 def __drawOutliers(df, columns, outputFile):
 
@@ -31,30 +48,34 @@ def __drawOutliers(df, columns, outputFile):
     outlier_scores = getOutlierScores(dataDf)
 
     if len(columns) < 2:
-        log.info('Can not generate outlier plot due to insufficient number of attributes (<2)')
+        log.info(
+            "Can not generate outlier plot due to insufficient number of attributes (<2)"
+        )
         return
     elif len(columns) == 2:
         plotdf = dataDf
     else:
-        plotdf = pd.DataFrame(PCA(2).fit(dataDf).transform(dataDf), columns=['component_1', 'component_2'])
+        plotdf = pd.DataFrame(
+            PCA(2).fit(dataDf).transform(dataDf), columns=["component_1", "component_2"]
+        )
 
-    plotdf['outlier_scores'] = outlier_scores
+    plotdf["outlier_scores"] = outlier_scores
 
     if plotdf is not None and len(plotdf) > 1:
         fig, ax = plt.subplots()
         x = plotdf.columns[0]
         y = plotdf.columns[1]
-        sns.scatterplot(data=plotdf, x=x, y=y, hue='outlier_scores')
+        sns.scatterplot(data=plotdf, x=x, y=y, hue="outlier_scores")
 
-        ax.set_title('Scatter plot')
+        ax.set_title("Scatter plot")
         ax.set_xlabel(x)
         ax.set_ylabel(y)
 
         encoded = None
-        outPath = Path(Path(outputFile).parent, 'outlier_plot_' + x + '_' + y + '.png')
-        fig.savefig(outPath, format='png', bbox_inches='tight')
+        outPath = Path(Path(outputFile).parent, "outlier_plot_" + x + "_" + y + ".png")
+        fig.savefig(outPath, format="png", bbox_inches="tight")
         with open(outPath, "rb") as outFile:
-            encoded = base64.b64encode(outFile.read()).decode('utf-8')
+            encoded = base64.b64encode(outFile.read()).decode("utf-8")
     else:
         encoded = None
 
@@ -65,29 +86,38 @@ def clean(source_file, save_file, columns):
 
     outputFile = Path(save_file)
     dataDf = pd.read_csv(source_file)
-    log.info('Validating the input arguments.')
+    log.info("Validating the input arguments.")
 
-    if(dataDf[columns].shape[1] > Settings.col_limit_for_outlier_plot):
-        log.info('Too many variables to plot!! Please sleect the vaiables to plot using combinations argument.')
+    if dataDf[columns].shape[1] > Settings.col_limit_for_outlier_plot:
+        log.info(
+            "Too many variables to plot!! Please sleect the vaiables to plot using combinations argument."
+        )
         return
     elif (dataDf.shape[0] * dataDf.shape[1]) > int(Settings.cell_limit):
-        log.info('This file has ' + str(dataDf.shape[0] * dataDf.shape[1]) + ' cells.')
-        log.info('The maximum number of cell that can be passed to this pipeline is ' + str(Settings.cell_limit))
-        log.info('File too big to handle!! Please remove the columns with low coverage and try again.')
-        log.info('Refer to this link: https://ehr-qc-tutorials.readthedocs.io/en/latest/process.html#large-file-handling')
+        log.info("This file has " + str(dataDf.shape[0] * dataDf.shape[1]) + " cells.")
+        log.info(
+            "The maximum number of cell that can be passed to this pipeline is "
+            + str(Settings.cell_limit)
+        )
+        log.info(
+            "File too big to handle!! Please remove the columns with low coverage and try again."
+        )
+        log.info(
+            "Refer to this link: https://ehr-qc-tutorials.readthedocs.io/en/latest/process.html#large-file-handling"
+        )
         return
-    log.info('Validating complete!!')
+    log.info("Validating complete!!")
 
-    log.info('Removing outliers')
+    log.info("Removing outliers")
     df = dataDf[columns]
     if df is not None:
         if not df.empty:
             outlier_scores = getOutlierScores(df)
-            dataDf['outlier_scores'] = outlier_scores
+            dataDf["outlier_scores"] = outlier_scores
             correctedDf = dataDf[dataDf.outlier_scores == 1]
-            correctedDf.drop(columns=['outlier_scores'], inplace=True)
+            correctedDf.drop(columns=["outlier_scores"], inplace=True)
 
-    log.info('Saving the corrected file')
+    log.info("Saving the corrected file")
     if (correctedDf is not None) and (not correctedDf.empty):
         dirPath = Path(outputFile).parent
         if not os.path.exists(dirPath):
@@ -99,100 +129,122 @@ def visualise(source_file, save_file, columns):
 
     outputFile = Path(save_file)
     dataDf = pd.read_csv(source_file)
-    log.info('Validating the input arguments.')
+    log.info("Validating the input arguments.")
 
-    if(columns and (dataDf[columns].shape[1] > Settings.col_limit_for_outlier_plot)):
-        log.info('Too many variables to plot!! Please sleect the vaiables to plot using combinations argument.')
+    if columns and (dataDf[columns].shape[1] > Settings.col_limit_for_outlier_plot):
+        log.info(
+            "Too many variables to plot!! Please sleect the vaiables to plot using combinations argument."
+        )
         return
     elif (dataDf.shape[0] * dataDf.shape[1]) > int(Settings.cell_limit):
-        log.info('This file has ' + str(dataDf.shape[0] * dataDf.shape[1]) + ' cells.')
-        log.info('The maximum number of cell that can be passed to this pipeline is ' + str(Settings.cell_limit))
-        log.info('File too big to handle!! Please remove the columns with low coverage and try again.')
-        log.info('Refer to this link: https://ehr-qc-tutorials.readthedocs.io/en/latest/process.html#large-file-handling')
+        log.info("This file has " + str(dataDf.shape[0] * dataDf.shape[1]) + " cells.")
+        log.info(
+            "The maximum number of cell that can be passed to this pipeline is "
+            + str(Settings.cell_limit)
+        )
+        log.info(
+            "File too big to handle!! Please remove the columns with low coverage and try again."
+        )
+        log.info(
+            "Refer to this link: https://ehr-qc-tutorials.readthedocs.io/en/latest/process.html#large-file-handling"
+        )
         return
-    log.info('Validating complete!!')
+    log.info("Validating complete!!")
 
     start = time.time()
 
-    log.info('Generating outlier report')
-    doc.asis('<!DOCTYPE html>')
-    with tag('html'):
-        doc.asis('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">')
-        with tag('body'):
+    log.info("Generating outlier report")
+    doc.asis("<!DOCTYPE html>")
+    with tag("html"):
+        doc.asis(
+            '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">'
+        )
+        with tag("body"):
             if columns:
-                with tag('div'):
-                    with tag('h1'):
-                        doc.asis('<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>')
-                        with tag('span', klass='fs-4', style="margin: 10px;"):
-                            text('Outliers plot')
-                            log.info('Generating outlier plot for columns: ' + str(columns))
+                with tag("div"):
+                    with tag("h1"):
+                        doc.asis(
+                            '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>'
+                        )
+                        with tag("span", klass="fs-4", style="margin: 10px;"):
+                            text("Outliers plot")
+                            log.info(
+                                "Generating outlier plot for columns: " + str(columns)
+                            )
                     fig = __drawOutliers(dataDf, columns, outputFile)
                     if fig:
-                        with tag('div', style="float: left;"):
-                            doc.asis('<img src=\'data:image/png;base64,{}\'>'.format(fig))
+                        with tag("div", style="float: left;"):
+                            doc.asis("<img src='data:image/png;base64,{}'>".format(fig))
                 doc.asis('<div style="clear:both;"></div>')
             doc.asis('<div style="clear:both;"></div>')
-            with tag('div'):
-                with tag('span', klass='description', style="margin: 10px; color:grey"):
-                    with tag('small'):
-                        text('Time taken to generate this report: ' + str(round(time.time() - start, 2)) + ' Sec')
+            with tag("div"):
+                with tag("span", klass="description", style="margin: 10px; color:grey"):
+                    with tag("small"):
+                        text(
+                            "Time taken to generate this report: "
+                            + str(round(time.time() - start, 2))
+                            + " Sec"
+                        )
             doc.asis('<div style="clear:both;"></div>')
-    log.info('Report generated!!')
+    log.info("Report generated!!")
 
-    log.info('Writing output file')
+    log.info("Writing output file")
     dirPath = Path(outputFile).parent
     if not os.path.exists(dirPath):
         os.makedirs(dirPath)
-    with open(outputFile, 'w') as output:
+    with open(outputFile, "w") as output:
         output.write(doc.getvalue())
-    log.info('Completed writing to output file!!')
+    log.info("Completed writing to output file!!")
 
 
-def run(source_file, save_file, columns=[]):
+def run(source_file, save_file, action, columns=[]):
     if columns:
-        if args.action[0] == 'clean':
+        if action == "clean":
             clean(source_file=source_file, save_file=save_file, columns=columns)
         else:
             visualise(source_file=source_file, save_file=save_file, columns=columns)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    import logging
-    import sys
-    import argparse
+    parser = argparse.ArgumentParser(
+        description="Outlier graphs using IRT ensemble technique"
+    )
 
-    log = logging.getLogger("EHR-QC")
-    log.setLevel(logging.INFO)
-    format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    parser.add_argument(
+        "source_file", nargs=1, default="./data.csv", help="Source data file path"
+    )
 
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setFormatter(format)
-    log.addHandler(ch)
+    parser.add_argument(
+        "save_file", nargs=1, default="./", help="Path of the file to store the output"
+    )
 
-    log.info("Parsing command line arguments")
+    parser.add_argument(
+        "action",
+        nargs=1,
+        default="visualise",
+        help="Action to perform [visualise, clean]",
+    )
 
-    parser = argparse.ArgumentParser(description='Outlier graphs using IRT ensemble technique')
-
-    parser.add_argument('source_file', nargs=1, default='./data.csv',
-                        help='Source data file path')
-
-    parser.add_argument('save_file', nargs=1, default='./',
-                        help='Path of the file to store the output')
-
-    parser.add_argument('action', nargs=1, default='visualise',
-                        help='Action to perform [visualise, clean]')
-
-    parser.add_argument('-col', '--columns', nargs='*',
-                        help='Column names to be used for outlier detection - must have two or more column names (required for both actions i.e. visualise and clean).')
+    parser.add_argument(
+        "-col",
+        "--columns",
+        nargs="*",
+        help="Column names to be used for outlier detection - must have two or more column names (required for both actions i.e. visualise and clean).",
+    )
 
     args = parser.parse_args()
 
-    log.info('args.source_file: ' + str(args.source_file[0]))
-    log.info('args.save_file: ' + str(args.save_file[0]))
-    log.info('args.action: ' + str(args.action[0]))
-    log.info('args.columns: ' + str(args.columns))
+    log.info("args.source_file: " + str(args.source_file[0]))
+    log.info("args.save_file: " + str(args.save_file[0]))
+    log.info("args.action: " + str(args.action[0]))
+    log.info("args.columns: " + str(args.columns))
 
-    run(source_file=args.source_file[0], save_file=args.save_file[0], columns=args.columns)
+    run(
+        source_file=args.source_file[0],
+        save_file=args.save_file[0],
+        action=args.action[0],
+        columns=args.columns,
+    )
 
-    log.info('Done!!')
+    log.info("Done!!")
